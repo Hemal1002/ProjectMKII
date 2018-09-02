8a8d042816a72cd2b576ada2a95cbc1ea35d8453
Hemal1002 <42447171+Hemal1002@users.noreply.github.com>
2018-09-02 20:19:01 +0200

    Started Unit Testing For Jobs.

--- Sprint1Final/Sprint1Final.Tests/UnitTest1.cs
+++ Sprint1Final/Sprint1Final.Tests/UnitTest1.cs
@@ -1,14 +1,66 @@
-﻿using System;
+﻿using Sprint1Final.Models;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Sprint1Final.Tests
+namespace Sprint1Final.Models.Tests
 {
-    [TestClass]
+    [TestClass()]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod()]
+        public void checkAbLoadTest()
         {
+            Job obj = new Job();
+
+            obj.CWeight = 2000;
+            obj.CWidth = 200;
+            obj.CLength = 1800;
+            obj.CHeight = 400;
+
+            string exp = "Normal";
+
+            string act = obj.checkAbLoad();
+
+            Assert.AreEqual(exp, act);
         }
+
+        [TestMethod()]
+        public void calcETATest()
+        {
+            Job obj = new Job();
+
+
+            obj.x = "Abnormal";
+            obj.Dist = "40";
+
+            DateTime exp = new DateTime(0,0,0,01,0,0);
+
+            DateTime act = obj.calcETA();
+
+            Assert.AreEqual(exp, act);
+
+
+        }
+
+        [TestMethod()]
+        public void calcBasicCostTest()
+        {
+            Job obj = new Job();
+
+
+            obj.x = "Abnormal";
+            obj.Dist = "40";
+
+            DateTime exp = new DateTime(0, 0, 0, 01, 0, 0);
+
+            DateTime act = obj.calcETA();
+
+            Assert.AreEqual(exp, act);
+
+
+        }
+
+
     }
 }
+
